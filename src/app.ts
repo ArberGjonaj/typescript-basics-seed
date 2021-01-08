cd58d18dd583d910fc1bc5b01176106122897a60
Arber Gjonaj <agjonaj@ikub.local>
2021-01-08 14:30:07 +0100

    Arrow functions and implicit returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Ahoy TypeScript!');
+const pizzas = [{ name: "Pepperoni", toppings: ["pepperoni"] }];
+
+const mappedPizzas = pizzas.map((pizza, index) => {
+  return pizza.name.toUpperCase();
+});
+console.log(mappedPizzas);
+
+const pizza = {
+  name: "Blazing Inferno",
+  getName:()=>pizza.name
+  ,
+};
+
+console.log(pizza.getName());
