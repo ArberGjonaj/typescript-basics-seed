4e043611116aa875eb2459ccfe3d4512198d985d
Arber Gjonaj <agjonaj@ikub.local>
2021-01-11 14:36:24 +0100

    Setters and Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
+
+class Sizes {
+   constructor(private sizes: string[]){}
+   
+   set availableSizes(sizes:string[]){
+    this.sizes=sizes
+   }
+   get availableSizes(){
+   return this.sizes
+   }
+}
+
+const sizes= new Sizes(["small","medium"])
+//invoke getter
+console.log(sizes.availableSizes)
+//invoke setter
+sizes.availableSizes=['medium','large']
+//invoke getter
+console.log(sizes.availableSizes)
 class Pizza {
 public toppings:string[]=[];
 
