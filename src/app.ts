31a3be39e965e2352dbf8b51ede0ee44d9ee2c54
Arber Gjonaj <agjonaj@ikub.local>
2021-01-11 14:47:19 +0100

    Classes and Inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,27 @@
-
 class Sizes {
-   constructor(private sizes: string[]){}
-   
-   set availableSizes(sizes:string[]){
-    this.sizes=sizes
-   }
-   get availableSizes(){
-   return this.sizes
-   }
+  constructor(private sizes: string[]) {}
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+  get availableSizes() {
+    return this.sizes;
+  }
 }
 
-const sizes= new Sizes(["small","medium"])
-//invoke getter
-console.log(sizes.availableSizes)
-//invoke setter
-sizes.availableSizes=['medium','large']
-//invoke getter
-console.log(sizes.availableSizes)
-class Pizza {
-public toppings:string[]=[];
+class Pizza extends Sizes {
+  public toppings: string[] = [];
 
-   constructor(readonly name:string){}
+  constructor(readonly name: string,sizes: string[]) {
+    super(sizes);
+  }
 
-public   addTopping(topping:string){
-   this.toppings.push(topping);
-   }
-
+  public addTopping(topping: string) {
+    this.toppings.push(topping);
+  }
 }
 
-
-
-const pizza = new Pizza('Pepperoni');
-
-pizza.addTopping('pepperoni');
-console.log(pizza.name)
+const pizza = new Pizza("Pepperoni",["small","medium"]);
+console.log(pizza.availableSizes)
+pizza.addTopping("pepperoni");
+console.log(pizza.name);
