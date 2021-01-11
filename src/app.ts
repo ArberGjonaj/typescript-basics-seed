443276f3b49407d95c85be2f899d9efc40876055
Arber Gjonaj <agjonaj@ikub.local>
2021-01-11 10:43:23 +0100

    Typed Functions and Default Params

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => {
-  if (y) {
-    return x * y;
-  }
-  return x;
+sumOrder = (x, y = 1) => {
+  return x * y;
 };
 
-const sum: number = sumOrder(25);
+const sum: number = sumOrder(25,5);
 
 console.log(`Total sum: ${sum}`);
