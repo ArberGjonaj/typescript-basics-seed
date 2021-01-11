e5da1928d67f464b0ed7438f2f78b72741550dea
Arber Gjonaj <agjonaj@ikub.local>
2021-01-11 11:58:48 +0100

    Interfaces with Function Types

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 interface Pizza {
   name: string;
   sizes: string[];
+  getAvailableSizes():string[];
 }
 
 interface Pizzas {
   data:Pizza[]
 }
+type getAvailableSizes =()=>string[]
 let pizza: Pizza;
 
 function createPizza(name: string, sizes: string[]): Pizza {
   return {
     name,
     sizes,
+    getAvailableSizes(){
+      return this.sizes;
+    }
   };
 }
 
